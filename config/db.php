214dd80ec540be487c07b6d68050bd8c0f1d428d
wecangonow <wecangonow@sina.com>
2015-11-23 23:01:36 +0800

    fixed untracked files

--- config/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=10.1.9.102;dbname=egg',
-    'username' => 'root',
-    'password' => '123456',
-    'charset' => 'utf8',
-];
